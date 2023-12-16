46d730cb0133791dcde609bb6feb9c46eb11c2af
Kyrylo Shykhalov <kshykhalov@kse.org.ua>
2023-12-16 20:33:57 +0200

    first_part
    IN prograss without startup logic

--- src/main.py
+++ src/main.py
@@ -1,22 +1,46 @@
-first_player = 'X'
-second_player = 'O'
+import pygame
+import sys
 
-game_field = [
-    [" ", " ", " "],
-    [" ", " ", " "],
-    [" ", " ", " "]
-]
+WIDTH, HEIGHT = 800, 600
+FPS = 60
+WHITE = (255, 255, 255)
 
-# first player is first to make a move
-current_player = first_player
+TITLE = "Arkanoid"
+pygame.display.set_caption(TITLE)
 
-row, column = input_move()
-game_field[row][column] = current_player
-print_field()
-winner = check_winner()
+Background = pygame.image.load("images\\1 (1).jpg")
 
-if winner is not None:
-    congratulate_player(winner)
-    quit()
+class Paddle(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("images\\platform.png")
+        self.rect = self.image.get_rect(center=(WIDTH // 2, HEIGHT - 30))
 
-current_player = first_player if current_player == second_player else second_player
+    def update(self):
+        self.rect.centerx = pygame.mouse.get_pos()[0]
+        self.rect.clamp_ip(screen.get_rect())
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.radius = 10
+        self.image = pygame.image.load("images\\ball.png")
+        self.rect = self.image.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+        self.speed = pygame.Vector2(5, 5)
+
+    def update(self):
+        self.rect.move_ip(self.speed)
+        if self.rect.left < 0 or self.rect.right > WIDTH:
+            self.speed.x *= -1
+        if self.rect.top < 0 or self.rect.bottom > HEIGHT:
+            self.speed.y *= -1
+        if self.rect.colliderect(paddle):
+            self.speed.y *= -1
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+
+paddle = Paddle()
+ball = Ball()
+
